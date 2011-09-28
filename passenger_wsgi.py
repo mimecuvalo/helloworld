db8613fe4f5bbe821b1bd50523757912fbc01e9e
Mime Čuvalo <mimecuvalo@gmail.com>
2011-09-28 02:03:45 +0200

    remove whitespace

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,7 +1,7 @@
 import os, sys
 
-INTERP = "/usr/bin/python" 
-if sys.executable != INTERP: os.execl(INTERP, INTERP, *sys.argv) 
+INTERP = "/usr/bin/python"
+if sys.executable != INTERP: os.execl(INTERP, INTERP, *sys.argv)
 
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 
