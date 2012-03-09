ac4e35b4ea519399ea02a2f4a1ceb077a62e973d
Mime Čuvalo <mimecuvalo@gmail.com>
2012-03-09 17:42:40 +0100

    blargh, more quote fixing

--- controllers/base.py
+++ controllers/base.py
@@ -154,7 +154,7 @@ class BaseHandler(tornado.web.RequestHandler):
     return string.replace("'", "\\'").replace('"', '\\"')
 
   def js_in_html_escape(self, string):
-    return tornado.escape.xhtml_escape(string).replace("'", "&#39;").replace('"', '\\"')
+    return tornado.escape.xhtml_escape(string).replace("'", "'").replace('"', '\\"').replace('&quot;', '\\"')
 
   def bidi(self, string):
     # modified from goog.i18n.bidi
