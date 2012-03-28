8be60fa68924c5dec8c35b140f046103bc7f2a22
Mime Čuvalo <mimecuvalo@gmail.com>
2012-03-29 01:16:10 +0200

    update browserid

--- static/js/site.js
+++ static/js/site.js
@@ -9,8 +9,8 @@ hw.login = function(event, el) {
 
   login.innerHTML = login.getAttribute('data-logging-in');
 
-  navigator.id.getVerifiedEmail(function(assertion) {
-    if (assertion) {
+  navigator.id.get(function(assertion) {
+    if (assertion !== null) {
       // This code will be invoked once the user has successfully
       // selected an email address they control to sign in with.
       var callback = function(xhr) {
