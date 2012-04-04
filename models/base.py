fe8fb4ae53d67241424bee18ab12e5f6dc2125a3
Mime Čuvalo <mimecuvalo@gmail.com>
2012-04-04 02:21:20 +0200

    fix to thread model

--- models/base.py
+++ models/base.py
@@ -69,7 +69,7 @@ class content_remote(Model):
                 'date_updated': None,
                 'comments_count': 0,
                 'comments_updated': None,
-                'thread': None,
+                'thread': '',
                 'avatar': '',
                 'link': '',
                 'view': '',}
