0eebb8f22e78732925b5a6fec6c935b9c1c6fb99
Mime Čuvalo <mimecuvalo@gmail.com>
2012-04-05 03:00:08 +0200

    do correct smtp send

--- logic/smtp.py
+++ logic/smtp.py
@@ -37,17 +37,17 @@ def send(handler, subject, to, content):
 
 def comment(handler, from_username, to_email, content_url, comment, this_user_mentioned=False):
   if this_user_mentioned:
-    send(handler, handler.locale.translate('%(user)s made a post mentioning you in it!' % { "user": from_username }), to_email, handler.locale.translate(u"""
+    send(handler, handler.locale.translate('%(user)s made a post mentioning you in it!') % { "user": from_username }, to_email, handler.locale.translate("""
       <a href="%(url)s">View the post here!</a>
     """) % { "url": content_url })
   else:
-    send(handler, handler.locale.translate('%(user)s made a comment on your post.' % { "user": from_username }), to_email, handler.locale.translate(u"""
+    send(handler, handler.locale.translate('%(user)s made a comment on your post.') % { "user": from_username }, to_email, handler.locale.translate("""
       %(comment)s<br><a href="%(url)s">View the post here!</a>
     """) % { "comment": comment, "url": content_url })
 
 def follow(handler, from_username, to_email, blog):
   follow = handler.nav_url(host=True, section='api') + '?op=follow&amp;from_email=1&amp;user=' + tornado.escape.url_escape(blog)
-  send(handler, handler.locale.translate('%(user)s started following you!' % { "user": from_username }), to_email, handler.locale.translate(u"""
+  send(handler, handler.locale.translate('%(user)s started following you!') % { "user": from_username }, to_email, handler.locale.translate("""
     <a href="%(blog)s">View their blog.</a><br><br>
     If you like their stuff, you can <a href="%(follow)s">follow them here.</a>
   """) % { "blog": blog, "follow": follow })
