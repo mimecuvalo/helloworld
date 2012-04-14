bf39a497e04012faa24bf10d88f7c4d83d8d775b
Mime Čuvalo <mimecuvalo@gmail.com>
2012-04-14 04:23:23 +0200

    tweak twitter

--- controllers/twitter.py
+++ controllers/twitter.py
@@ -46,9 +46,10 @@ class TwitterHandler(BaseHandler,
 
     self.user = self.get_author_user()
     access_token = json.loads(self.user.twitter)
-    status =    content_logic.ellipsize(content_remote.strip_html(self.get_argument('title', '')), 19, including_dots=True) \
-        + ':' + content_logic.ellipsize(content_remote.strip_html(self.get_argument('view', '')), 99, including_dots=True) \
-        + ':' + self.get_argument('url');
+    status =    content_logic.ellipsize(content_remote.strip_html(self.get_argument('title', '')), 18, including_dots=True) \
+        + (': ' if self.get_argument('view', '') else '')
+        + content_logic.ellipsize(content_remote.strip_html(self.get_argument('view', '')), 99, including_dots=True) \
+        + ' ' + self.get_argument('url');
     self.twitter_request(
             "/statuses/update",
             self.status_update_result,
