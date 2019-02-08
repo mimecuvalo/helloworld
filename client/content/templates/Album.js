b5586e85897265861e9545ddb97ca28cbf039c34
Mime Čuvalo <mimecuvalo@gmail.com>
2019-02-08 10:11:20 -0800

    album: split off thumb class into separate file

--- client/content/templates/Album.js
+++ client/content/templates/Album.js
@@ -1,16 +1,13 @@
 import classNames from 'classnames';
 import { contentUrl } from '../../../shared/util/url_factory';
-import { defineMessages, F, injectIntl } from '../../../shared/i18n';
+import DelayLoadedThumb from './Thumb';
+import { F } from '../../../shared/i18n';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
 import { Link } from 'react-router-dom';
-import React, { Component, PureComponent } from 'react';
+import React, { PureComponent } from 'react';
 import styles from './Album.module.css';
 import UserContext from '../../app/User_Context';
-
-const messages = defineMessages({
-  thumbnail: { msg: 'thumbnail' },
-});
 
 @graphql(
   gql`
@@ -83,65 +80,5 @@ class Album extends PureComponent {
     );
   }
 }
-
-class Thumb extends Component {
-  constructor() {
-    super();
-
-    this.image = React.createRef();
-
-    this.state = {
-      isAboveFold: false,
-      isLoaded: false,
-    };
-  }
-  componentDidMount() {
-    window.addEventListener('scroll', this.maybeLoadImage, { passive: true });
-    window.addEventListener('resize', this.maybeLoadImage, { passive: true });
-
-    this.maybeLoadImage();
-  }
-
-  componentWillUnmount() {
-    this.removeEventListeners();
-  }
-
-  removeEventListeners() {
-    window.removeEventListener('scroll', this.maybeLoadImage);
-    window.removeEventListener('resize', this.maybeLoadImage);
-  }
-
-  maybeLoadImage = () => {
-    if (this.image.current.getBoundingClientRect().top < window.innerHeight + 175 /* fudge factor */) {
-      this.setState({ isAboveFold: true });
-      this.removeEventListeners();
-    }
-  };
-
-  handleThumbLoad = evt => {
-    if (!this.state.aboveFold) {
-      this.setState({ isLoaded: true });
-    }
-  };
-
-  render() {
-    const item = this.props.item;
-    const thumbAltText = this.props.intl.formatMessage(messages.thumbnail);
-    const src = this.state.isAboveFold ? item.thumb : '/img/pixel.gif';
-
-    return (
-      <img
-        src={src}
-        ref={this.image}
-        alt={thumbAltText}
-        onLoad={this.handleThumbLoad}
-        className={classNames('hw-invisible-slow-transition', styles.thumb, {
-          'hw-invisible': !this.state.isLoaded || !this.state.isAboveFold,
-        })}
-      />
-    );
-  }
-}
-const DelayLoadedThumb = injectIntl(Thumb);
 
 export default Album;
---
+++ client/content/templates/Thumb.js
@@ -0,0 +1,70 @@
+import classNames from 'classnames';
+import { defineMessages, injectIntl } from '../../../shared/i18n';
+import React, { Component } from 'react';
+import styles from './Album.module.css';
+
+const messages = defineMessages({
+  thumbnail: { msg: 'thumbnail' },
+});
+
+class Thumb extends Component {
+  constructor() {
+    super();
+
+    this.image = React.createRef();
+
+    this.state = {
+      isAboveFold: false,
+      isLoaded: false,
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.maybeLoadImage, { passive: true });
+    window.addEventListener('resize', this.maybeLoadImage, { passive: true });
+
+    this.maybeLoadImage();
+  }
+
+  componentWillUnmount() {
+    this.removeEventListeners();
+  }
+
+  removeEventListeners() {
+    window.removeEventListener('scroll', this.maybeLoadImage);
+    window.removeEventListener('resize', this.maybeLoadImage);
+  }
+
+  maybeLoadImage = () => {
+    if (this.image.current.getBoundingClientRect().top < window.innerHeight + 175 /* fudge factor */) {
+      this.setState({ isAboveFold: true });
+      this.removeEventListeners();
+    }
+  };
+
+  handleThumbLoad = evt => {
+    if (!this.state.aboveFold) {
+      this.setState({ isLoaded: true });
+    }
+  };
+
+  render() {
+    const item = this.props.item;
+    const thumbAltText = this.props.intl.formatMessage(messages.thumbnail);
+    const src = this.state.isAboveFold ? item.thumb : '/img/pixel.gif';
+
+    return (
+      <img
+        src={src}
+        ref={this.image}
+        alt={thumbAltText}
+        onLoad={this.handleThumbLoad}
+        className={classNames('hw-invisible-slow-transition', styles.thumb, {
+          'hw-invisible': !this.state.isLoaded || !this.state.isAboveFold,
+        })}
+      />
+    );
+  }
+}
+
+export default injectIntl(Thumb);
