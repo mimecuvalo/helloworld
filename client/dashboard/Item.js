caf2e3d92048d82e9e68238704c2eb667f4c1c88
Mime Čuvalo <mimecuvalo@gmail.com>
2019-02-12 16:22:28 -0800

    dashboard: add some sane css defaults for p and ul/ol elements

--- client/dashboard/Item.js
+++ client/dashboard/Item.js
@@ -1,13 +1,15 @@
+import classNames from 'classnames';
 import Footer from './Footer';
 import Header from './Header';
 import React, { PureComponent } from 'react';
+import styles from './Item.module.css';
 
 class Item extends PureComponent {
   render() {
     const contentRemote = this.props.contentRemote;
 
     return (
-      <section className="hw-item">
+      <section className={classNames('hw-item', styles.item)}>
         <Header contentRemote={contentRemote} />
         <div dangerouslySetInnerHTML={{ __html: contentRemote.view }} />
         <Footer contentRemote={contentRemote} />
