301ea572651c45050892d7072fdc17ced6d5d37a
Mime Čuvalo <mimecuvalo@gmail.com>
2019-10-08 23:25:08 -0700

    env: fix setup local dev env for packages

--- bin/setup_local_dev_environment.sh
+++ bin/setup_local_dev_environment.sh
@@ -1,8 +1,10 @@
 cd packages/hello-world-editor
 npm link
 
-cd packages/social-butterfly
+cd ../../packages/social-butterfly
 npm link
+
+cd ../../
 
 npm link hello-world-editor
 npm link social-butterfly
