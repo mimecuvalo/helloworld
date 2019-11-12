54d7e78816c3054f61ab6c028e56d38716337013
Mime Čuvalo <mimecuvalo@gmail.com>
2019-11-12 15:32:09 -0800

    resolvers: nitpick not logged in should through auth error, not forbidden error

--- server/data/graphql/resolvers/authorization.js
+++ server/data/graphql/resolvers/authorization.js
@@ -1,12 +1,20 @@
+import { AuthenticationError, ForbiddenError } from 'apollo-server-express';
 import authorization from '../../../util/authorization';
-import { ForbiddenError } from 'apollo-server-express';
 import { skip } from 'graphql-resolvers';
 
 export const isAuthenticated = (parent, args, { currentUser }) =>
-  authorization.isAuthenticated(currentUser) ? skip : new ForbiddenError('Not logged in.');
+  authorization.isAuthenticated(currentUser) ? skip : new AuthenticationError('Not logged in.');
 
 export const isAuthor = (parent, args, { currentUser }) =>
-  authorization.isAuthor(currentUser) ? skip : new ForbiddenError('I call shenanigans.');
+  authorization.isAuthenticated(currentUser)
+    ? authorization.isAuthor(currentUser)
+      ? skip
+      : new ForbiddenError('I call shenanigans.')
+    : new AuthenticationError('Not logged in.');
 
 export const isAdmin = (parent, args, { currentUser }) =>
-  authorization.isAdmin(currentUser) ? skip : new ForbiddenError('I call shenanigans.');
+  authorization.isAuthenticated(currentUser)
+    ? authorization.isAdmin(currentUser)
+      ? skip
+      : new ForbiddenError('I call shenanigans.')
+    : new AuthenticationError('Not logged in.');
