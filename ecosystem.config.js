c2eda6d0fc5995e34e5a97046ba7631bd5b60ea8
Mime Čuvalo <mimecuvalo@gmail.com>
2021-02-23 22:30:48 -0800

    add more defaults to ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,6 +2,20 @@ module.exports = {
   _comment: "This is used by pm2 on production.",
   apps : [{
     name   : "helloworld",
-    script : "npm run serve:prod"
+    script : "npm run serve:prod",
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    args: '',
+    instances: 1,
+    autorestart: true,
+    error_file: '/dev/stderr',
+    out_file: '/dev/stdout',
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development',
+    },
+    env_production: {
+      NODE_ENV: 'production',
+    },
   }]
 }
