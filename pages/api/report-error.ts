1ec270ee67b0028cc05d0792420e896657a4ad9b
Mime Čuvalo <mimecuvalo@gmail.com>
2022-10-31 11:10:26 -0700

    fix logging

--- pages/api/report-error.ts
+++ pages/api/report-error.ts
@@ -2,6 +2,6 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   // Hook up a error logging service here on the backend if desired (e.g. Sentry).
-  console.log('Error:', JSON.parse(req.query.data || req.body.data));
+  console.log('Error:', req.query.data ? JSON.parse(req.query.data as string) : req.body.data);
   res.status(204).send('');
 }
