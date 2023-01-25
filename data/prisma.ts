5936f2f23e5b17e6d023f9a89d307d9819286b26
Mime Čuvalo <mimecuvalo@gmail.com>
2023-01-25 08:50:30 -0800

    reduce noise in logs for prisma

--- data/prisma.ts
+++ data/prisma.ts
@@ -13,7 +13,7 @@ declare global {
 const prisma =
   global.prisma ||
   new PrismaClient({
-    log: ['query'],
+    log: process.env.NODE_ENV === 'development' ? ['query'] : undefined,
   });
 export default prisma;
 if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
