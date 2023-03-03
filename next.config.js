294b3f5810f10948c93684ef8309fc4dfab4ede7
Mime Čuvalo <mimecuvalo@gmail.com>
2023-03-03 08:30:51 -0800

    next.config: modularize mui icon imports for speed; add scrollRestoration

--- next.config.js
+++ next.config.js
@@ -29,6 +29,21 @@ const nextConfig = {
     styledComponents: true,
   },
 
+  experimental: {
+    externalDir: true,
+    scrollRestoration: true,
+
+    // N.B. This is super important so that our bundles don't slow down the whole site.
+    modularizeImports: {
+      '@mui/material': {
+        transform: '@mui/material/{{member}}',
+      },
+      '@mui/icons-material': {
+        transform: '@mui/icons-material/{{member}}',
+      },
+    },
+  },
+
   i18n: {
     locales: ['en', 'fr'],
     defaultLocale: 'en',
