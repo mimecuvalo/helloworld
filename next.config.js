871b7f2decc33989e648de30ede4eec015e45407
Mime Čuvalo <mimecuvalo@gmail.com>
2023-03-07 21:20:16 -0800

    next.config: comment out formatjs no-parser optimization, was breaking with swc

--- next.config.js
+++ next.config.js
@@ -101,8 +101,9 @@ const nextConfig = {
     );
 
     if (!dev) {
+      // TODO(mime): try to re-enable this but with SWC it was breaking, was only working with Babel.
       // https://formatjs.io/docs/guides/advanced-usage#react-intl-without-parser-40-smaller
-      config.resolve.alias['@formatjs/icu-messageformat-parser'] = '@formatjs/icu-messageformat-parser/no-parser';
+      // config.resolve.alias['@formatjs/icu-messageformat-parser'] = '@formatjs/icu-messageformat-parser/no-parser';
     }
 
     return config;
