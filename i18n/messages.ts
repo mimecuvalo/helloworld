bdd1883e55d0f61cb6082675b7ee334b70b8e983
Mime Čuvalo <mimecuvalo@gmail.com>
2025-08-06 16:11:58 +0200

    i18n, try to fix again

--- i18n/messages.ts
+++ i18n/messages.ts
@@ -1,23 +1,26 @@
 import { GetStaticPropsContext } from 'next';
 import fs from 'fs/promises';
 import path from 'path';
-import enJson from '../i18n-compiled-lang/en.json';
 
-export default async function loadIntlMessages({ locale, defaultLocale }: GetStaticPropsContext): Promise<any> {
-  if (locale === defaultLocale) {
-    return enJson;
+const compiledLangCache: { [key: string]: any } = {};
+
+export default async function loadIntlMessages({ locale }: GetStaticPropsContext): Promise<any> {
+  if (!locale) return null;
+
+  if (compiledLangCache[locale]) {
+    return compiledLangCache[locale];
   }
 
-  if (locale !== defaultLocale) {
-    const languagePath = path.join(process.cwd(), `i18n-compiled-lang/${locale}.json`);
-    try {
-      const contents = await fs.readFile(languagePath, 'utf-8');
-      return JSON.parse(contents);
-    } catch (error) {
-      console.error('Could not load compiled language files. Please run "yarn i18n:compile" first"');
-      console.error(error);
-    }
+  const languagePath = path.join(process.cwd(), `i18n-compiled-lang/${locale}.json`);
+  try {
+    const contents = await fs.readFile(languagePath, 'utf-8');
+    const messages = JSON.parse(contents);
+    compiledLangCache[locale] = messages;
+    return messages;
+  } catch (error) {
+    console.error('Could not load compiled language files. Please run "yarn i18n:compile" first"');
+    console.error(error);
   }
 
-  return {};
+  return null;
 }
