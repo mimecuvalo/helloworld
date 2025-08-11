0c94fba3b0051a7059acf83794810e0a3db160b3
Mime Čuvalo <mimecuvalo@gmail.com>
2025-08-11 13:21:46 +0200

    build test

--- prisma/client/edge.js
+++ prisma/client/edge.js
@@ -254,7 +254,7 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": false,
+  "postinstall": true,
   "inlineDatasources": {
     "db": {
       "url": {
--- prisma/client/index.js
+++ prisma/client/index.js
@@ -255,7 +255,7 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": false,
+  "postinstall": true,
   "inlineDatasources": {
     "db": {
       "url": {
