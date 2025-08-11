c83606887db1da2765e1f07e3f68caff3ebe9f89
Mime Čuvalo <mimecuvalo@gmail.com>
2025-08-11 22:03:12 +0200

    aws-sdk

--- pages/api/upload-file.ts
+++ pages/api/upload-file.ts
@@ -3,7 +3,8 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { MAX_FILE_SIZE } from 'util/constants';
 import { User } from '@prisma/client';
 import authenticate from '@/application/authentication';
-import aws from 'aws-sdk';
+import { createPresignedPost } from '@aws-sdk/s3-presigned-post';
+import { S3Client } from '@aws-sdk/client-s3';
 import mime from 'mime/lite';
 
 export default authenticate(async function handler(req: NextApiRequest, res: NextApiResponse, currentUser: User) {
@@ -14,23 +15,19 @@ export default authenticate(async function handler(req: NextApiRequest, res: Nex
   };
 
   try {
-    const s3 = new aws.S3(AWS_CONFIG);
+    const s3Client = new S3Client(AWS_CONFIG);
 
-    aws.config.update({
-      ...AWS_CONFIG,
-      signatureVersion: 'v4',
-    });
-
+    const contentType = mime.getType(req.query.file as string) || 'application/octet-stream';
     const extraFields = {
       'Content-Disposition': 'inline',
       'Cache-Control': 'public, max-age=31536000',
-      'Content-Type': mime.getType(req.query.file as string),
+      'Content-Type': contentType,
     };
 
-    const post = await s3.createPresignedPost({
-      Bucket: process.env.S3_AWS_S3_BUCKET_NAME,
+    const post = await createPresignedPost(s3Client, {
+      Bucket: process.env.S3_AWS_S3_BUCKET_NAME!,
+      Key: `${currentUser.username}/${req.query.file}`,
       Fields: {
-        key: `${currentUser.username}/${req.query.file}`,
         ...extraFields,
       },
       Expires: 60, // seconds
